2c84adce849744db8ea86b34db111247db0f8e97
Filipp Kosenko <filipp.kosenko@idapgroup.com>
2024-06-12 12:56:30 +0300

    HMT-93 -> Placeholder position fixed. Added cell stretching on the Favorites screen.

--- lib/ui/widgets/bookmark_button.dart
+++ lib/ui/widgets/bookmark_button.dart
@@ -5,15 +5,23 @@ import 'package:help_my_truck/const/colors.dart';
 import 'package:help_my_truck/services/API/favorites_provider.dart';
 
 class BookmarkButton extends StatefulWidget {
+  
   FavoritesProvider? provider;
   String? integrationId;
   String? type;
+  final double size;
   final bool isFixedState;
   void Function(String)? voidCallback;
 
-  BookmarkButton(this.integrationId, this.type, this.provider, this.voidCallback,
-      this.isFixedState,
-      {super.key});
+  BookmarkButton(
+    this.size,
+    this.integrationId,
+    this.type,
+    this.provider,
+    this.voidCallback,
+    this.isFixedState, {
+    super.key,
+  });
 
   @override
   State<BookmarkButton> createState() => _BookmarkButtonState();
@@ -58,34 +66,38 @@ class _BookmarkButtonState extends State<BookmarkButton> {
 
   @override
   Widget build(BuildContext context) {
-    return IconButton(
-      padding: const EdgeInsets.all(0),
-      icon: Icon(
-        size: 28,
-        _isBookmarked ? Icons.bookmark : Icons.bookmark_border_outlined,
-        color: ColorConstants.onSurfaceWhite,
-      ),
-      onPressed: () {
-        if (item != null) {
-          widget.provider?.change(item?.id ?? 0);
-          changeIconState();
-        } else {
-          final integrationId = widget.integrationId;
-          final type = widget.type;
-          if (integrationId != null && type != null) {
-            widget.provider?.createContentfulItem(integrationId, type);
-            loadItem().then((value) {
-             widget.provider?.change(item?.id ?? 0);
-             changeIconState();
-            });
+    return SizedBox(
+      height: 36,
+      child: IconButton(
+        iconSize: widget.size,
+        alignment: Alignment.center,
+        padding: EdgeInsets.zero,
+        icon: Icon(
+          _isBookmarked ? Icons.bookmark : Icons.bookmark_border_outlined,
+          color: ColorConstants.onSurfaceWhite,
+        ),
+        onPressed: () {
+          if (item != null) {
+            widget.provider?.change(item?.id ?? 0);
+            changeIconState();
+          } else {
+            final integrationId = widget.integrationId;
+            final type = widget.type;
+            if (integrationId != null && type != null) {
+              widget.provider?.createContentfulItem(integrationId, type);
+              loadItem().then((value) {
+                widget.provider?.change(item?.id ?? 0);
+                changeIconState();
+              });
+            }
           }
-        }
-
-        final callback = widget.voidCallback;
-        if (callback != null) {
-          callback(widget.integrationId ?? '');
-        }
-      },
+      
+          final callback = widget.voidCallback;
+          if (callback != null) {
+            callback(widget.integrationId ?? '');
+          }
+        },
+      ),
     );
   }
 }
