b3de01cfcfd084f4ad4b8a3bcb2e9071fef05203
Ermakov Alexander <alexandr.ermakov@idapgroup.com>
2024-06-22 00:45:15 +0300

    HMT-41 -> Send button removed

--- lib/ui/comments_flow/comments_screen.dart
+++ lib/ui/comments_flow/comments_screen.dart
@@ -328,20 +328,20 @@ class _CommentsScreenState extends State<CommentsScreen> {
               );
             },
           ),
-          Positioned(
-            right: 0,
-            bottom: 8,
-            child: SendButton(
-              controller: _controller,
-              onTap: () {
-                if (_controller.text.isNotEmpty &&
-                    !RegExp(r'^\s*$').hasMatch(_controller.text)) {
-                  widget.viewModel.addComment(_controller.text);
-                  _controller.text = '';
-                }
-              },
-            ),
-          ),
+          // Positioned(
+          //   right: 0,
+          //   bottom: 8,
+          //   child: SendButton(
+          //     controller: _controller,
+          //     onTap: () {
+          //       if (_controller.text.isNotEmpty &&
+          //           !RegExp(r'^\s*$').hasMatch(_controller.text)) {
+          //         widget.viewModel.addComment(_controller.text);
+          //         _controller.text = '';
+          //       }
+          //     },
+          //   ),
+          // ),
         ],
       ),
     );
